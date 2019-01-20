e5f7a3577255fe41e40fac99705d44a9b5cf2a9b
Ahmed Hamdy <hamdyjs@gmail.com>
2019-01-20 16:14:52 +0200

    Implement first basic parser function

--- link.go
+++ link.go
@@ -1,6 +1,10 @@
 package link
 
-import "io"
+import (
+	"io"
+
+	"golang.org/x/net/html"
+)
 
 // Link type
 type Link struct {
@@ -10,5 +14,35 @@ type Link struct {
 
 // Parse will take an io.Reader and return a slice of links
 func Parse(r io.Reader) ([]Link, error) {
-	return nil, nil
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+
+	links := make([]Link, 0)
+	var f func(node *html.Node)
+	f = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "a" {
+			var href string
+			for _, attr := range node.Attr {
+				if attr.Key == "href" {
+					href = attr.Val
+					break
+				}
+			}
+			text := node.FirstChild.Data
+
+			links = append(links, Link{href, text})
+		}
+
+		if node.FirstChild != nil {
+			f(node.FirstChild)
+		}
+		if node.NextSibling != nil {
+			f(node.NextSibling)
+		}
+	}
+	f(doc)
+
+	return links, nil
 }
